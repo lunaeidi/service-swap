21a636954bacca32c83c66376f07d5e4a59e5bff
Abhigya Maskay <abhigya@enterpriseesupport.com>
2018-08-03 13:51:30 -0400

    Added Skills model and migration files

---
+++ server/models/skill.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Skill = sequelize.define('Skill', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    wantOrOffer: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {});
+  Skill.associate = function(models) {
+    Skill.belongsTo(models.User)
+  };
+  return Skill;
+};
